76eb71382676e04e0d7cefb5e9b0db98fdbd122d
adodoCoder <alavropulido@my.whccd.edu>
2025-01-15 20:07:01 -0800

    added more green comments

--- COMP003A.LectureActivity1/Program.cs
+++ COMP003A.LectureActivity1/Program.cs
@@ -1,4 +1,10 @@
-﻿// namespace level
+﻿/* namespace level
+ * Author: [Alvaro Pulido]
+ * Course: [COMP003A]
+ * Faculty: Jonathan Cruz
+ * Purpose: This application demonstrates the basic structure of a .NET console application.
+ * */
+
 namespace COMP003A.LectureActivity1
 {
     // class level
